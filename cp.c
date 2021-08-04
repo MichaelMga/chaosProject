26dcf4a3caf1223924222e7da48e1b08dab3fcc3
michaelmga <middleweightsoul@gmail.com>
2021-08-04 23:54:18 +0200

    adding a keyboard event

--- cp.c
+++ cp.c
@@ -8,11 +8,13 @@
  int main(int argc, char *argv[])
  {
    SDL_Window *window = NULL;
-   
+   SDL_Surface *windowSurface = NULL;
    SDL_Surface *image1 = NULL;
-   SDL_Surface *image2 = NULL;
+   SDL_Surface *image2 = NULL;  
+   SDL_Surface *currentImage = image1;
    
-   
+ 
+    
    image1 = IMG_Load("resources/warrior.png");
    image2 = IMG_Load("resources/hello.png");
    
@@ -20,6 +22,10 @@
    
    window = SDL_CreateWindow("Chaos project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
    
+   
+      windowSurface = SDL_GetWindowSurface(window);
+      
+      
    int isRunning = 1;
    
    SDL_Event ev;
@@ -32,10 +38,24 @@
        if(ev.type == SDL_QUIT){
           isRunning = 0;
        } else if (ev.type == SDL_KEYDOWN) {
-           
-           
+            
+            switch(ev.key.keysm.sym){
+                  
+              case SDLK_1 :
+                currentImage = image1;
+              
+              case SDLK_2 :
+                currentImage = image2;      
+               
+            
+           }
          }
        } 
+       
+       SDL_BlitSurface(currentImage,NULL, windowSurface, NULL );
+       
+       SDL_UpdateWindowSurface(window);
+       
     }
     
     return 0;
